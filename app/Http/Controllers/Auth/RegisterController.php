5816a86e3be8603f41d04785ddfbbfb519671e99
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-27 08:52:14 +0700

    Update with role

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,10 +62,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => bcrypt($data['password']),
+            'password' => bcrypt($data['password'])
         ]);
+        $user->role = 'customer';
+        $user->save();
+        return $user;
     }
 }
