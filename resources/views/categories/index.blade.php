93fc78228d67f4183fe31b951a71abac23a09eb8
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-27 13:46:07 +0700

    Create index category

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h3>Category</h3>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <td>Title</td>
+              <td>Parent</td>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($categories as $category)
+              <tr>
+                <td>{{ $category->title }}</td>
+                <td>{{ $category->parent ? $category->parent->title : '' }}</td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+</div>
+@endsection
