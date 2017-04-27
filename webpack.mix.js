c6929f6396bbfeceadd6db59872748dd93de7f9f
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-27 09:48:12 +0700

    Update mix asset

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,17 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+mix.styles([
+   'public/css/vendor/normalize.css',
+   'public/css/vendor/videojs.css'
+], 'public/css/all.css');
+mix.scripts([
+   '../../../node_modules/jquery/dist/jquery.js',
+   '../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js'
+], 'public/js/all.js');
+mix.version([
+   'css/all.css',
+   'js/all.js'
+]);
+mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
+mix.copy('node_modules/font-awesome/fonts', 'public/build/fonts');
