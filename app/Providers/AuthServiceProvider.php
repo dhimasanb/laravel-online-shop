866ec91844f543e951a3aa348a7980272e5eec87
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-27 09:46:50 +0700

    Create gate admin & customer

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,12 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('admin-access', function ($user) {
+            return $user->role == 'admin';
+        });
+
+        Gate::define('customer-access', function ($user) {
+            return $user->role == 'customer';
+        });
     }
 }
