4be54dab5b73ec18cc7cc38b4d2817c631a05740
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-27 14:05:10 +0700

    Add search bar

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -4,6 +4,13 @@
   <div class="row">
     <div class="col-md-12">
       <h3>Category</h3>
+      {!! Form::open(['url' => 'categories', 'method'=>'get', 'class'=>'form-inline']) !!}
+        <div class="form-group {!! $errors->has('q') ? 'has-error' : '' !!}">
+          {!! Form::text('q', isset($q) ? $q : null, ['class'=>'form-control', 'placeholder' => 'Type category..']) !!}
+          {!! $errors->first('q', '<p class="help-block">:message</p>') !!}
+        </div>
+        {!! Form::submit('Search', ['class'=>'btn btn-primary']) !!}
+      {!! Form::close() !!}
         <table class="table table-hover">
           <thead>
             <tr>
