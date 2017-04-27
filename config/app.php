ec47902e33fb9e3e8fc1346b2fe522d88d83bc50
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-27 15:42:22 +0700

    Add laracasts flash

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Laracasts\Flash\FlashServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -228,7 +229,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-
+        'Flash' => Laracasts\Flash\Flash::class,
     ],
 
 ];
