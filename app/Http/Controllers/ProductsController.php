829e524a55abc3a0cdf4d92b2b89cb463514fca5
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-28 08:46:32 +0700

    Add construct

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('role:admin');
+    }
     /**
      * Display a listing of the resource.
      *
