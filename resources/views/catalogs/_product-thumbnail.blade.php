c5994ac9339247f6f7a2207baaab969bdcfaa475
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-29 17:17:34 +0700

    Create partial product thumbnail view

---
+++ resources/views/catalogs/_product-thumbnail.blade.php
@@ -0,0 +1,13 @@
+<h3>{{ $product->name }}</h3>
+<div class="thumbnail">
+  <img src="{{ $product->photo_path }}" class="img-rounded">
+    <p>Model: {{ $product->model }}</p>
+    <p>Harga: <strong>Rp{{ number_format($product->price, 2) }}</strong></p>
+    <p>Category:
+      @foreach ($product->categories as $category)
+        <span class="label label-primary">
+        <i class="fa fa-btn fa-tags"></i>
+        {{ $category->title }}</span>
+      @endforeach
+    </p>
+</div>
