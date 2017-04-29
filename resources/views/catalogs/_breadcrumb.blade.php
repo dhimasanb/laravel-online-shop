d5724328a0f3b6ceb88c9ac1dd8c8dd65d0e85b1
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-29 20:26:04 +0700

    Add sort button

--- resources/views/catalogs/_breadcrumb.blade.php
+++ resources/views/catalogs/_breadcrumb.blade.php
@@ -1,8 +1,16 @@
 <ol class="breadcrumb">
   @if(!is_null($current_category))
-    <li>Kategori: <a href="{{ url('/catalogs?cat=' . $current_category->id) }}">
-      {{ $current_category->title }}</a></li>
+    <li>Kategori: <a href="{{ url('/catalogs?cat=' . $current_category->id) }}">{{ $current_category->title }}</a></li>
   @else
     <li>Kategori: Semua Produk</a></li>
   @endif
+  <span class="pull-right">Urutkan Harga:
+  <a href="{{ appendQueryString(['sort'=>'price', 'order'=>'asc']) }}"
+    class="btn btn-default btn-xs
+    {{ isQueryStringEqual(['sort'=>'price', 'order'=>'asc']) ? 'active' : ''}}"
+    >Termurah</a> |
+  <a href="{{ appendQueryString(['sort'=>'price', 'order'=>'desc']) }}"
+    class="btn btn-default btn-xs
+    {{ isQueryStringEqual(['sort'=>'price', 'order'=>'desc']) ? 'active' : ''}}"
+    >Termahal</a>
 </ol>
