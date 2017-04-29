a9fcd43483324db3ae4d7e57a1f94c385e633f39
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-29 20:25:49 +0700

    Add sort function

--- app/Http/Controllers/CatalogsController.php
+++ app/Http/Controllers/CatalogsController.php
@@ -10,15 +10,26 @@ class CatalogsController extends Controller
 {
     public function index(Request $request)
     {
-      if ($request->has('cat')) {
-        $cat = $request->get('cat');
-        $category = Category::findOrFail($cat);
-        // we use this to get product from current category and its child
-        $products = Product::whereIn('id', $category->related_products_id)
-        ->paginate(4);
-      } else {
-        $products = Product::paginate(4);
-      }
-      return view('catalogs.index', compact('products', 'cat'));
+      $q = $request->get('q');
+        if ($request->has('cat')) {
+            $cat = $request->get('cat');
+            $category = Category::findOrFail($cat);
+            // we use this to get product from current category and its child
+            $products = Product::whereIn('id', $category->related_products_id)
+                ->where('name', 'LIKE', '%'.$q.'%');
+        } else {
+            $products = Product::where('name', 'LIKE', '%'.$q.'%');
+        }
+
+        if ($request->has('sort')) {
+            $sort = $request->get('sort');
+            $order = $request->has('order') ? $request->get('order') : 'asc';
+            $field = in_array($sort, ['price', 'name']) ? $request->get('sort') : 'price';
+            $products = $products->orderBy($field, $order);
+        }
+
+        $products = $products->paginate(4);
+
+        return view('catalogs.index', compact('products', 'q', 'cat', 'category', 'sort', 'order'));
     }
 }
