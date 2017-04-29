eab9ab86fc58fe3d26d562030c1312eaefde803a
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-29 17:46:23 +0700

    Update index

--- app/Http/Controllers/CatalogsController.php
+++ app/Http/Controllers/CatalogsController.php
@@ -9,7 +9,15 @@ class CatalogsController extends Controller
 {
     public function index()
     {
+      if ($request->has('cat')) {
+        $cat = $request->get('cat');
+        $category = Category::findOrFail($cat);
+        // we use this to get product from current category and its child
+        $products = Product::whereIn('id', $category->related_products_id)
+        ->paginate(4);
+      } else {
         $products = Product::paginate(4);
-        return view('catalogs.index', compact('products'));
+      }
+      return view('catalogs.index', compact('products', 'cat'));
     }
 }
