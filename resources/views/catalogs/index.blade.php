3a8f208cf711c59fe4c6acf89d8fdb9fee948120
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-29 17:46:51 +0700

    Include category panel

--- resources/views/catalogs/index.blade.php
+++ resources/views/catalogs/index.blade.php
@@ -4,7 +4,7 @@
   <div class="container">
     <div class="row">
       <div class="col-md-3">
-        Sidebar
+        @include('catalogs._category-panel')
       </div>
       <div class="col-md-9">
         <div class="row">
