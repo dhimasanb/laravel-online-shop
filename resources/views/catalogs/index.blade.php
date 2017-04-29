82220a23dd368f182755bb1f8b242c9a50ca3efc
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-29 17:17:20 +0700

    Create catalogs index view

---
+++ resources/views/catalogs/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        Sidebar
+      </div>
+      <div class="col-md-9">
+        <div class="row">
+          <div class="col-md-12">
+            <ol class="breadcrumb">
+              <li>Kategori: Semua Produk</li>
+            </ol>
+          </div>
+          @foreach ($products as $product)
+          <div class="col-md-6">
+            @include('catalogs._product-thumbnail', ['product' => $product])
+          </div>
+          @endforeach
+
+          <div class="pull-right">
+            {!! $products->links() !!}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
