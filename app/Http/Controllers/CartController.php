31d41fbe59318cd51e580daf79947387252710f8
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-30 06:56:14 +0700

    Create addProduct

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class CartController extends Controller
 {
-    //
+    public function addProduct(Request $request)
+     {
+         $this->validate($request, [
+             'product_id' => 'required|exists:products,id',
+             'quantity' => 'required|integer|min:1'
+         ]);
+
+         $product = Product::find($request->get('product_id'));
+         $quantity = $request->get('quantity');
+
+         $cart = $request->cookie('cart', []);
+         if (array_key_exists($product->id, $cart)) {
+             $quantity += $cart[$product->id];
+         }
+         $cart[$product->id] = $quantity;
+         return redirect('catalogs')
+             ->withCookie(cookie()->forever('cart', $cart));
+     }
 }
