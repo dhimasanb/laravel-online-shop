02bf1f74300c333b5f3832517acbdf76b2ac4dd8
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-29 20:51:33 +0700

    Include customer feature

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,6 +62,9 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
+
+                       @include('layouts._customer-feature', ['partial_view'=>'layouts._cart-menu-bar'])
+
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
