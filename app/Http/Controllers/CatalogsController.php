4bf2d8d787081a28f332d3b5eccffcc489432114
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-29 17:16:49 +0700

    Add index

--- app/Http/Controllers/CatalogsController.php
+++ app/Http/Controllers/CatalogsController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class CatalogsController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::paginate(4);
+        return view('catalogs.index', compact('products'));
+    }
 }
