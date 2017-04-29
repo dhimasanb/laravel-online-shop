b46b6ef20808b52e7f33262130b96cca7cfe7715
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-29 18:38:04 +0700

    Update search function

--- app/Http/Controllers/CatalogsController.php
+++ app/Http/Controllers/CatalogsController.php
@@ -10,15 +10,16 @@ class CatalogsController extends Controller
 {
     public function index(Request $request)
     {
+      $q = $request->get('q');
       if ($request->has('cat')) {
         $cat = $request->get('cat');
         $category = Category::findOrFail($cat);
         // we use this to get product from current category and its child
         $products = Product::whereIn('id', $category->related_products_id)
-        ->paginate(4);
+        ->where('name', 'LIKE', '%'.$q.'%');
       } else {
-        $products = Product::paginate(4);
+        $products = Product::where('name', 'LIKE', '%'.$q.'%');
       }
-      return view('catalogs.index', compact('products', 'cat', 'category'));
+      return view('catalogs.index', compact('products', 'cat', 'category', 'q'));
     }
 }
