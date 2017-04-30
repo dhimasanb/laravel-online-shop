b9c6c767e22acb16c9d7553687cac928fd9370a7
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-30 20:45:22 +0700

    Add guestCheckout

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\CheckoutLoginRequest;
+use Illuminate\Support\MessageBag;
+use App\User;
 
 class CheckoutController extends Controller
 {
@@ -23,6 +25,18 @@ class CheckoutController extends Controller
         }
 
         return $this->authenticatedCheckout($email, $password);
+    }
+
+    protected function guestCheckout($email)
+    {
+        // check if user exist, if so, ask login
+        if ($user = User::where('email', $email)->first()) {
+            if ($user->hasPassword()) {
+            // (A) Logic ketika email ada di DB dengan password
+            }
+            // (B) Logic ketika email di DB tanpa password
+        }
+        // (C) Logic ketika email tidak ada di DB
     }
 
     protected function authenticatedCheckout($email, $password)
