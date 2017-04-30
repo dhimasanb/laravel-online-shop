ca81d7764131b72b21b207ab1241de7a31d2ca2f
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-30 19:58:21 +0700

    Allow null quantity

--- database/migrations/2017_04_30_011652_create_carts_table.php
+++ database/migrations/2017_04_30_011652_create_carts_table.php
@@ -17,7 +17,7 @@ class CreateCartsTable extends Migration
           $table->increments('id');
           $table->integer('user_id')->unsigned();
           $table->integer('product_id')->unsigned();
-          $table->integer('quantity');
+          $table->integer('quantity')->nullable();
           $table->foreign('product_id')->references('id')->on('products');
           $table->foreign('user_id')->references('id')->on('users');
           $table->timestamps();
