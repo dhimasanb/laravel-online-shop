ad0725bc4b488627a145c91ac696182c2f4bdd0d
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-30 20:00:54 +0700

    Create route get post checkout

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,5 @@ Route::post('cart', 'CartController@addProduct');
 Route::get('cart', 'CartController@show');
 Route::delete('cart/{product_id}', 'CartController@removeProduct');
 Route::put('cart/{product_id}', 'CartController@changeQuantity');
+Route::get('checkout/login', 'CheckoutController@login');
+Route::post('checkout/login', 'CheckoutController@postLogin');
