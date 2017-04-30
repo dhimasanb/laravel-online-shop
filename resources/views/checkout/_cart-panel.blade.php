3622739b51ac539af7fadd87f1464b31c1ee3e17
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-30 20:33:46 +0700

    Create partial cart panel view

---
+++ resources/views/checkout/_cart-panel.blade.php
@@ -0,0 +1,34 @@
+<div class="panel panel-default">
+    <div class="panel-heading">Cart</div>
+    <div class="panel-body">
+    <table class="table table-condensed">
+      <thead>
+        <tr>
+          <th style="width:50%">Produk</th>
+          <th style="width:20%">Jumlah</th>
+          <th style="width:30%">Harga</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($cart->details() as $order)
+        <tr>
+          <td data-th="Produk">{{ $order['detail']['name'] }}</td>
+          <td data-th="Jumlah" class="text-center">{{ $order['quantity'] }}</td>
+          <td data-th="Harga" class="text-right">{{ number_format($order['detail']['price']) }}</td>
+        </tr>
+        <tr>
+          <td data-th="Subtotal">Subtotal</td>
+          <td data-th="Subtotal" class="text-right" colspan="2">Rp{{ number_format($order['subtotal']) }}</td>
+        </tr>
+
+        @endforeach
+      </tbody>
+      <tfoot>
+        <tr>
+          <td><strong>Total</strong></td>
+          <td class="text-right" colspan="2"><strong>Rp{{ number_format($cart->totalPrice()) }}</strong></td>
+        </tr>
+      </tfoot>
+    </table>
+    </div>
+</div>
