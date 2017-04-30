329c23d06d0ae64ce28393a7c847e20ce5195bdf
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-30 20:17:55 +0700

    Create login & post login

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
 {
-    //
+    public function login()
+    {
+        return view('checkout.login');
+    }
+
+    public function postLogin() { }
 }
