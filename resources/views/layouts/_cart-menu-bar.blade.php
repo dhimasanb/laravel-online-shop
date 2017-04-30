980ba91ddae7705100de089f0ef7fdcef43e7670
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-30 07:26:11 +0700

    Add total product

--- resources/views/layouts/_cart-menu-bar.blade.php
+++ resources/views/layouts/_cart-menu-bar.blade.php
@@ -1,1 +1,1 @@
-<li><a href="{{ url('/cart') }}"><i class="fa fa-shopping-cart"></i> Cart</a></li>
+<li><a href="{{ url('/cart') }}"><i class="fa fa-shopping-cart"></i> Cart {{ $cart->totalProduct() > 0 ? '(' . $cart->totalProduct() . ')' : ''}}</a></
