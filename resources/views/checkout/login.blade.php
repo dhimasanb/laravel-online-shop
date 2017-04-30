4b6024c2626a97299b5db4d70c30e0db1090bcdc
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-01 06:25:21 +0700

    Add status message

--- resources/views/checkout/login.blade.php
+++ resources/views/checkout/login.blade.php
@@ -8,6 +8,11 @@
       <div class="panel panel-default">
           <div class="panel-heading">Login atau Checkout tanpa mendaftar</div>
           <div class="panel-body">
+              @if (session('status'))
+                <div class="alert alert-success">
+                  {{ session('status') }}
+                </div>
+              @endif
               @include('checkout._login-form')
           </div>
       </div>
