0d9505880eaf73105dadeead964bff68a049484a
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-01 22:02:24 +0700

    Create regencies

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Regency;
 
 class AddressController extends Controller
 {
-    //
+    public function regencies(Request $request)
+    {
+        $this->validate($request, [
+            'province_id' => 'required|exists:provinces,id'
+        ]);
+
+        return Regency::where('province_id', $request->get('province_id'))
+            ->get();
+    }
 }
