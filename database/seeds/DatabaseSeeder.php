a14126466b932c3788292bfb9008dddefdb2ceaf
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-01 18:43:34 +0700

    Call addresses seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(UsersSeeder::class);
         $this->call(ProductsSeeder::class);
+        $this->call(AddressesSeeder::class);
     }
 }
