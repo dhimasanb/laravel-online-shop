59d958690f8f03be55c180c30818d65d656af1f9
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-01 19:52:48 +0700

    Create checkout address view

---
+++ resources/views/checkout/address.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  @include('checkout._step')
+  <div class="row">
+    <div class="col-xs-8">
+      <div class="panel panel-default">
+          <div class="panel-heading">Alamat Pengiriman</div>
+          <div class="panel-body">
+            @include('checkout._address-new-form')
+          </div>
+      </div>
+    </div>
+    <div class="col-xs-4">
+      @include('checkout._cart-panel')
+    </div>
+  </div>
+</div>
+@endsection
