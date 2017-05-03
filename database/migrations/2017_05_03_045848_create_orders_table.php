9b58fbbcb0a0055d825e05c18b9e3b2445374b15
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-03 12:05:29 +0700

    Update with table order details

--- database/migrations/2017_05_03_045848_create_orders_table.php
+++ database/migrations/2017_05_03_045848_create_orders_table.php
@@ -15,8 +15,32 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('address_id')->unsigned();
+            $table->string('status')->default('waiting-payment');
+            $table->string('bank');
+            $table->string('sender');
+            $table->decimal('total_payment', 18, 2);
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('address_id')->references('id')->on('addresses');
         });
+
+        Schema::create('order_details', function (Blueprint $table) {
+          $table->increments('id');
+          $table->integer('order_id')->unsigned();
+          $table->integer('product_id')->unsigned();
+          $table->integer('quantity')->unsigned();
+          $table->decimal('price', 10, 2)->unsigned();
+          $table->decimal('fee', 10, 2)->unsigned();
+          $table->decimal('total_price', 18, 2);
+          $table->timestamps();
+
+          $table->foreign('order_id')->references('id')->on('orders');
+          $table->foreign('product_id')->references('id')->on('products');
+        });
+
     }
 
     /**
@@ -26,6 +50,7 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('order_details');
         Schema::dropIfExists('orders');
     }
 }
