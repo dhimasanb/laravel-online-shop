de1c391d30e5572507ba886750dae9488ca6a927
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-04 01:41:08 +0700

    Update handle

--- app/Http/Middleware/CheckoutPaymentStepDone.php
+++ app/Http/Middleware/CheckoutPaymentStepDone.php
@@ -13,8 +13,12 @@ class CheckoutPaymentStepDone
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
-        return $next($request);
-    }
+     public function handle($request, Closure $next)
+     {
+         if (!session()->has('order')) {
+            return redirect('checkout/payment');
+         }
+         
+         return $next($request);
+     }
 }
