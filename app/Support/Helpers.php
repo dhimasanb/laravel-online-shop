68603a2dde569a1ad4c0d7d2d7ef57289c71b422
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-03 07:36:23 +0700

    Create bank helper

--- app/Support/Helpers.php
+++ app/Support/Helpers.php
@@ -10,3 +10,12 @@ function appendQueryString($params) {
 function isQueryStringEqual($params) {
     return !array_diff($params, Request::all());
 }
+
+/** Bank list for <select> element */
+function bankList() {
+    $result = [];
+    foreach (config('bank-accounts') as $account => $detail) {
+        $result[$account] = $detail['title'];
+    }
+    return $result;
+}
