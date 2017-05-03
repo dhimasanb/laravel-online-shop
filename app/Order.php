3c16e6d31f2a0e29f7113c7ff7690b4e8f27faa3
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-03 12:05:43 +0700

    Update

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = ['id', 'user_id', 'address_id', 'bank', 'sender', 'status', 'total_payment'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function details()
+    {
+        return $this->hasMany('App\OrderDetail');
+    }
+
+    public function address()
+    {
+        return $this->belongsTo('App\Address');
+    }
 }
--- app/OrderDetail.php
+++ app/OrderDetail.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderDetail extends Model
 {
-    //
+    protected $table = 'order_details';
+
+    protected $fillable = ['id', 'order_id', 'product_id', 'quantity', 'price',
+        'fee', 'total_price'];
+
+    public function product()
+    {
+        return $this->belongsTo('App\Product');
+    }
+
+    public function order()
+    {
+        return $this->belongsTo('App\Order');
+    }
 }
