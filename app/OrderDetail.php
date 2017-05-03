2313b9891aa02c1390d513bb0fd365c5906f4e5d
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-03 12:15:56 +0700

    Create refresh total price method

--- app/OrderDetail.php
+++ app/OrderDetail.php
@@ -11,6 +11,24 @@ class OrderDetail extends Model
     protected $fillable = ['id', 'order_id', 'product_id', 'quantity', 'price',
         'fee', 'total_price'];
 
+    public static function boot() {
+        parent::boot();
+
+        static::saved(function($model) {
+            if ($model->total_price < 1) {
+                $model->refreshTotalPrice();
+            }
+            $model->order->refreshTotalPayment();
+        });
+    }
+
+    public function refreshTotalPrice()
+    {
+        $total_price = ($this->price + $this->fee) * $this->quantity;
+        $this->total_price = $total_price;
+        $this->save();
+    }
+
     public function product()
     {
         return $this->belongsTo('App\Product');
