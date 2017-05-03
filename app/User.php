238d6d269c96b7c0d4d3310babefb736f9d26df1
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-03 16:41:08 +0700

    Create address method

--- app/User.php
+++ app/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
     {
         return $this->password !== '';
     }
+
+    public function addresses()
+    {
+        return $this->hasMany('App\Address');
+    }
 }
