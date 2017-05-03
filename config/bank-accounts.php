c02da6b929ba0eb5f385d1a9446e95c868055149
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-03 07:36:10 +0700

    Create config bank account

---
+++ config/bank-accounts.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'bca' => [
+        'title' => 'BCA',
+        'bank' => 'BCA',
+        'name' => 'PT Reselia',
+        'number' => '45454545'
+    ],
+    'bni' => [
+        'title' => 'BNI',
+        'bank' => 'BNI',
+        'name' => 'PT Reselia',
+        'number' => '787687678'
+    ],
+    'mandiri' => [
+        'title' => 'Mandiri',
+        'bank' => 'Mandiri',
+        'name' => 'PT Reselia',
+        'number' => '23223232'
+    ],
+    'atm-bersama' => [
+        'title' => 'ATM Bersama',
+        'bank' => 'Mandiri',
+        'name' => 'PT Reselia',
+        'number' => '23223232'
+    ],
+];
