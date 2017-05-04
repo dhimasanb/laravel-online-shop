067dae1eda36b22a14e5ed553987c36f81790d69
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-04 15:22:59 +0700

    Create orders relation

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Address');
     }
+
+    public function orders()
+    {
+        return $this->hasMany('App\Order');
+    }
 }
