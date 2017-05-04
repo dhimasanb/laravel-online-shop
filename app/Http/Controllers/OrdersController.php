e5ce4609a4105a79d10c3563925a06379f5eec52
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-04 13:20:40 +0700

    Update with orderby

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -19,21 +19,25 @@ class OrdersController extends Controller
      * @return \Illuminate\Http\Response
      */
      public function index(Request $request)
-     {
+    {
         $status = $request->get('status');
-
+        $orders = Order::where('status', 'LIKE', '%'.$status.'%');
         if ($request->has('q')) {
-          $q = $request->get('q');
-          $orders = $orders->where(function($query) use ($q) {
-              $query->where('id', $q)
-                ->orWhereHas('user', function($user) use ($q) {
-                $user->where('name', 'LIKE', '%'.$q.'%');
-              });
-          });
+            $q = $request->get('q');
+            $orders = $orders->where(function($query) use ($q) {
+                $query->where('id', $q)
+                    ->orWhereHas('user', function($user) use ($q) {
+                        $user->where('name', 'LIKE', '%'.$q.'%');
+                    });
+            });
         }
-        $orders = $orders->paginate(10);
+        // dd($orders->toSql());
+
+        $orders = $orders->orderBy('updated_at', 'desc')
+            ->paginate(10);
+
         return view('orders.index', compact('orders', 'status', 'q'));
-     }
+    }
 
     /**
      * Show the form for creating a new resource.
